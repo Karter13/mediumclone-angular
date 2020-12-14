97cae392e2fbd68d91aad611fb0a9075a7512440
Karter13 <KarMiH1975@mail.ru>
2020-12-14 20:57:03 +0300

    created first Action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Store} from '@ngrx/store';
+
+import {registerAction} from '../../store/actions/register.action';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +14,10 @@ export class RegisterComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private store: Store
+  ) {
   }
 
   ngOnInit(): void {
@@ -27,6 +33,6 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.form.value);
+    this.store.dispatch(registerAction(this.form.value));
   }
 }
