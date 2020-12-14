d87e69560b7230804e28e88eb965d5ca4ca165bd
Karter13 <KarMiH1975@mail.ru>
2020-12-14 19:34:54 +0300

    created register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,12 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'mc-register',
+  selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
 }
