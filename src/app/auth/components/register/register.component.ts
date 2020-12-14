489fbf545aaf685037318c6f7e21d4cd1b8020cc
Karter13 <KarMiH1975@mail.ru>
2020-12-14 17:29:08 +0300

    created register.module and component

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+
+})
+export class RegisterComponent {
+
+}
