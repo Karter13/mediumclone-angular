b04c291be4c1d2ea747c2831d956375c228dd102
Karter13 <KarMiH1975@mail.ru>
2020-12-16 00:26:32 +0300

    created first services

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
 import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
 
 import {RegisterComponent} from 'src/app/auth/components/register/register.component';
-import { reducer } from './store/reducers';
+import {reducer} from './store/reducers';
 import {AuthService} from './services/auth.service';
+import {RegisterEffect} from './store/effects/register.effect';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent}
@@ -17,7 +19,8 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     ReactiveFormsModule,
-StoreModule.forFeature('auth', reducer)
+    StoreModule.forFeature('auth', reducer),
+    EffectsModule.forFeature([RegisterEffect])
   ],
   declarations: [RegisterComponent],
   providers: [AuthService]
