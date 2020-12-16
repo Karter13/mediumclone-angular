6f8884172868f592446ca9791c5e4d9c7f4c4a42
Karter13 <KarMiH1975@mail.ru>
2020-12-16 19:39:48 +0300

    created method login in auth.service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,10 +7,15 @@ import {RegisterRequestInterface} from '../types/registerRequest.interface';
 import {CurrentUserInterface} from '../../shared/types/currentUser.interface';
 import {environment} from '../../../environments/environment';
 import {AuthResponseInterface} from '../types/authResponse.interface';
+import {LoginRequestInterface} from '../types/loginRequest.interface';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {
+  }
+
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return  response.user;
   }
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
@@ -18,7 +23,16 @@ export class AuthService {
 
     return this.http.post<AuthResponseInterface>(url, data)
       .pipe(
-        map((response: AuthResponseInterface) => response.user)
+        map(this.getUser)
+      );
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users/login';
+
+    return this.http.post<AuthResponseInterface>(url, data)
+      .pipe(
+        map(this.getUser)
       );
   }
 }
