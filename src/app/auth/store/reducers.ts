7b700f2aa5f74f1d41714436bedf1f01ab2875a1
Karter13 <KarMiH1975@mail.ru>
2020-12-16 21:06:06 +0300

    created  login.component

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -2,6 +2,7 @@ import {Action, createReducer, on} from '@ngrx/store';
 
 import {AuthStateInterface} from '../types/authState.interface';
 import {registerAction, registerFailureAction, registerSuccessAction} from './actions/register.action';
+import {loginAction, loginFailureAction, loginSuccessAction} from './actions/login.action';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
@@ -12,25 +13,50 @@ const initialState: AuthStateInterface = {
 
 const authReducer = createReducer(
   initialState,
-  on(registerAction, (state): AuthStateInterface => ({
+  on(
+    registerAction, (state): AuthStateInterface => ({
       ...state,
       isSubmitting: true,
       validationErrors: null,
     })
   ),
-  on(registerSuccessAction, (state, action): AuthStateInterface => ({
+  on(
+    registerSuccessAction, (state, action): AuthStateInterface => ({
       ...state,
       isSubmitting: false,
       isLoggedIn: true,
       currentUser: action.currentUser
     })
   ),
-  on(registerFailureAction, (state, action): AuthStateInterface => ({
+  on(
+    registerFailureAction, (state, action): AuthStateInterface => ({
       ...state,
       isSubmitting: false,
       validationErrors: action.errors
     })
-  )
+  ),
+  on(
+    loginAction, (state): AuthStateInterface => ({
+      ...state,
+      isSubmitting: true,
+      validationErrors: null,
+    })
+  ),
+  on(
+    loginSuccessAction, (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      currentUser: action.currentUser,
+      isLoggedIn: true,
+    })
+  ),
+  on(
+    loginFailureAction, (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors,
+    })
+  ),
 );
 
 export function reducer(state: AuthStateInterface, action: Action) {
